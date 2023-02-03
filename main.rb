ad94ebdeaed3fef7a52e1ddf10ebce4b179d3fc5
sebalp100 <sebalp100@gmail.com>
2023-02-03 12:35:12 -0300

    Fix linters

--- main.rb
+++ main.rb
@@ -1,85 +1,124 @@
 def decode_char(char)
-  result = ""
-  
-  if char === ".-"  
-   result += "A"
-  elsif char === "-..."
-   result += "B"
-  elsif char === "-.-."
-   result += "C"
-  elsif char === "-.."
-   result += "D"
-  elsif char === "."
-   result += "E"
-  elsif char === "..-."
-   result += "F"
-  elsif char === "--."
-   result += "G"
-  elsif char === "...."
-   result += "H"
-  elsif char === ".."
-   result += "I"
-  elsif char === ".---"
-   result += "J"
-  elsif char === "-.-"
-   result += "K"
-  elsif char === ".-.."
-   result += "L"
-  elsif char === "--"
-   result += "M"
-  elsif char === "-."
-   result += "N"
-  elsif char === "---"
-   result += "O"
-  elsif char === ".--."
-   result += "P"
-  elsif char === "--.-"
-   result += "Q"
-  elsif char === ".-."
-   result += "R"
-  elsif char === "..."
-   result += "S"
-  elsif char === "-"
-   result += "T"
-  elsif char === "..-"
-   result += "U"
-  elsif char === "...-"
-   result += "V"
-  elsif char === ".--"
-   result += "W"
-  elsif char === "-..-"
-   result += "X"
-  elsif char === "-.--"
-   result += "Y"
-  elsif char === "--.."
-   result += "Z"
+  result = ''
+
+  case char
+  when '.-'
+    result += 'A'
+  when '-...'
+    result += 'B'
+  when '-.-.'
+    result += 'C'
+  when '-..'
+    result += 'D'
+  when '.'
+    result += 'E'
+  when '..-.'
+    result += 'F'
   end
-  
+  result
+end
+
+def decode_char2(char)
+  result = ''
+
+  case char
+  when '--.'
+    result += 'G'
+  when '....'
+    result += 'H'
+  when '..'
+    result += 'I'
+  when '.---'
+    result += 'J'
+  when '-.-'
+    result += 'K'
+  when '.-..'
+    result += 'L'
+
+  end
+  result
+end
+
+def decode_char3(char)
+  result = ''
+
+  case char
+  when '--'
+    result += 'M'
+  when '-.'
+    result += 'N'
+  when '---'
+    result += 'O'
+  when '.--.'
+    result += 'P'
+  when '--.-'
+    result += 'Q'
+  when '.-.'
+    result += 'R'
+  end
+  result
+end
+
+def decode_char4(char)
+  result = ''
+
+  case char
+  when '...'
+    result += 'S'
+  when '-'
+    result += 'T'
+  when '..-'
+    result += 'U'
+  when '...-'
+    result += 'V'
+  when '.--'
+    result += 'W'
+  when '-..-'
+    result += 'X'
+  end
+
+  result
+end
+
+def decode_char5(char)
+  result = ''
+
+  case char
+  when '-.--'
+    result += 'Y'
+  when '--..'
+    result += 'Z'
+  end
+
   result
 end
 
 def decode_word(word)
-  result = ""
-  
-  chars = word.split(" ")
+  result = ''
+
+  chars = word.split
   chars.each do |char|
     result += decode_char(char)
+    result += decode_char2(char)
+    result += decode_char3(char)
+    result += decode_char4(char)
+    result += decode_char5(char)
   end
-  
+
   result
 end
 
 def decode(message)
-  result = ""
-  
-  words = message.split("   ")
+  result = ''
+
+  words = message.split('   ')
   words.each do |word|
-    result += decode_word(word) + " "
+    result += "#{decode_word(word)} "
   end
-  
+
   result.strip
 end
 
-decoded = decode(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
+decoded = decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
 
 puts decoded
